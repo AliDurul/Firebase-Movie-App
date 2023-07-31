1a1404bf1bd54fe2655c9b28e7dcc56a55f0c6e8
Ali <alidrl26@gmail.com>
2023-07-31 19:35:36 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "gray-dark-main": "#23242a",
+        "gray-dark-second": "#28292d",
+        "gray-light": "#d3dce6",
+        "red-main": "#ff4b45",
+      },
+    },
+  },
+  plugins: [],
+  darkMode: 'class'
+};
+
+//! eğer tailwind'in default theme ayarları yerine kendimizinkini eklemek istemiyorsak mutlaka extend objesi içinde eklememiz gerekli yoksa tüm theme objesi değişir ve kendi eklediklerimiz dışındakileri kullanamayız.
